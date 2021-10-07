d957ea0840caae5662033ae5efabc57b9d1bb5e1
JackBucknerNRM <60416265+JackBucknerNRM@users.noreply.github.com>
2021-10-07 10:51:43 -0700

    added fourier transfomr code

---
+++ quant_gen_code/FFT_breaders_equation.jl
@@ -0,0 +1,40 @@
+module FFT_breaders_equation
+
+using FFTW
+
+mutable struct population
+    N::Int64 # abundance of popualtion 
+    m::Int64 # length of convolution kernel 
+    Vle::AbstractVector{Float64} # convolution kernel for Vle 
+    values::AbstractVector{Float64} # trait distribution 
+    gradient::AbstractVector{Float64} # survival probility under selection
+    grid::AbstractVector{Float64} # grid values for trait distribution
+end 
+
+function reproduction!(population)
+    dsn = population.values
+    v1 = DSP.conv(dsn, dsn)[1:2:(2*N+1)]
+    v1 = v1./sum(v1)
+    dsn_e = population.values[Vle]
+    v1 = DSP.conv(v1, dsn_e)[(m+1):(N+m+1)]
+    population.values = v1
+    return population
+end 
+
+
+function flow!(population, dsn)
+    populaiton.values .+= dsn
+    return population
+end 
+
+
+function selection!(population, gradient)
+    population.values .*= population.gradient
+    population.N *= sum(population.values)
+    population.values = population.values ./ sum(population.values)
+    return population
+end
+
+
+
+end # module
